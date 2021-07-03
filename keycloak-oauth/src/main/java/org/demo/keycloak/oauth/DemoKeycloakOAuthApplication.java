ade49449f5960e6535e80eaa56deae8cb934e8e3
Litovchenko Igor <nokitelinho@gmail.com>
2021-07-04 00:30:53 +0300

    Updated

---
+++ keycloak-oauth/src/main/java/org/demo/keycloak/oauth/DemoKeycloakOAuthApplication.java
@@ -0,0 +1,14 @@
+package org.demo.keycloak.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKeycloakOAuthApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKeycloakOAuthApplication.class, args);
+	}
+
+}
